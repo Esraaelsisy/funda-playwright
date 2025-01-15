a979fe5d9880235da62156b55d65488bcb035f4e
Esraaelsisy <esraa.elsisy@gmail.com>
2025-01-15 21:51:32 +0100

    Adding baseTest fixture for basic logging in with browser context and accepting cookies

---
+++ fixtures/baseTest.ts
@@ -0,0 +1,17 @@
+import { test as base, expect as baseExpect } from '@playwright/test';
+import { acceptCookies } from '../utilities/cookieHelper';
+
+export const test = base.extend<{}>({
+  context: async ({ browser }, use) => {
+    const context = await browser.newContext({
+      userAgent: process.env.USER_AGENT || 'Fallback User Agent', // Secure user agent
+    });
+    await use(context);
+  },
+  page: async ({ page }, use) => {
+    await page.goto('/');
+    await acceptCookies(page); 
+    await use(page); 
+  },
+});
+export { baseExpect as expect };
