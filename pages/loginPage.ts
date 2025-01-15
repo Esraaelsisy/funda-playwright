e6a087e99f220397887e4fff64c70aea633c07bb
Esraaelsisy <esraa.elsisy@gmail.com>
2025-01-15 22:30:55 +0100

    Adding LoginPage.ts file for Login page elements and functions

---
+++ pages/loginPage.ts
@@ -0,0 +1,23 @@
+import { Page, Locator } from '@playwright/test';
+
+export class LoginPage {
+  private page: Page;
+  private usernameInput: Locator;
+  private passwordInput: Locator;
+  private loginButton: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.usernameInput = page.locator('#UserName');
+    this.passwordInput = page.locator('#Password');
+    // Let me check if it's better to use type=submit?
+    this.loginButton = page.getByRole('button', { name: 'Log in' }); 
+  }
+
+
+  async login(username: string, password: string) {
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+  }
+}
