e84ddb0423306cf2ed2133eb442a0f00f0b931ea
Esraaelsisy <esraa.elsisy@gmail.com>
2025-01-19 23:17:03 +0100

    Updating Locators to use localization library for more dynamic usage in homePage.ts

--- pages/homePage.ts
+++ pages/homePage.ts
@@ -1,28 +1,33 @@
-import { Page, Locator } from "@playwright/test";
+import { Page, expect } from "@playwright/test";
+import { LocalizationHelper } from "../utilities/localizationHelper";
 
 export class HomePage {
   private page: Page;
 
   private locators = {
-    loginButton: () => this.page.getByRole("button", { name: "Inloggen" }),
+    loginButton: () => {
+      const login = LocalizationHelper.getLocalizedText("buttons", "login");
+      return this.page.getByRole("button", { name: login });
+    },
     accountMenu: () =>
       this.page.getByRole("button").filter({ hasText: "Account" }),
-    logoutButton: () =>
-      this.page.getByRole('link', { name: 'Uitloggen' })
+    logoutButton: (logoutLabel: string) => {
+      const logout = LocalizationHelper.getLocalizedText("buttons", "logout");
+      return this.page.getByRole("link", { name: logout });
+    },
   };
-
   constructor(page: Page) {
     this.page = page;
   }
   async navigateToLoginPage() {
     await this.locators.loginButton().click();
+    console.log("Login Page is opened.");
   }
-
   async openAccountMenu() {
     await this.locators.accountMenu().click();
+    console.log("Account menu is opened.");
   }
-
-  get accountMenu() {
-    return this.locators.accountMenu;
+  async isAccountMenuVisible() {
+    await expect(this.locators.accountMenu()).toBeVisible();
   }
 }
