d1b2c64dcbfd306b844c5f24567c3b2f4fdc8e7e
Esraaelsisy <esraa.elsisy@gmail.com>
2025-01-20 00:05:12 +0100

    Adding Headless configuration to easily toggle between headed and headless modes

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
     trace: "on-first-retry",
     screenshot: "only-on-failure",
     video: "retain-on-failure",
+    headless: process.env.HEADLESS !== "false",
   },
 
   /* Configure projects for major browsers */
