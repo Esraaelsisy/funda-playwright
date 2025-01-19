55e908a2e341aef5278f1cf462b44663555622ae
Esraaelsisy <esraa.elsisy@gmail.com>
2025-01-19 02:05:23 +0100

    updating the context in baseTest.ts fixture to have maximized window

--- fixtures/baseTest.ts
+++ fixtures/baseTest.ts
@@ -1,17 +1,18 @@
-import { test as base, expect as baseExpect } from '@playwright/test';
-import { acceptCookies } from '../utilities/cookieHelper';
+import { test as base, expect as baseExpect } from "@playwright/test";
+import { acceptCookies } from "../utilities/cookieHelper";
 
 export const test = base.extend<{}>({
   context: async ({ browser }, use) => {
     const context = await browser.newContext({
-      userAgent: process.env.USER_AGENT 
+      userAgent: process.env.USER_AGENT, 
+      viewport: { width: 1920, height: 1080 }
     });
     await use(context);
   },
   page: async ({ page }, use) => {
-    await page.goto('/');
-    await acceptCookies(page); 
-    await use(page); 
+    await page.goto("/");
+    await acceptCookies(page);
+    await use(page);
   },
 });
 export { baseExpect as expect };
