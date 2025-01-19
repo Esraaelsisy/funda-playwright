366966009e46fb4c57de22bcbb1d3517780ec354
Esraaelsisy <esraa.elsisy@gmail.com>
2025-01-19 23:18:24 +0100

    Adding house details page functions in houseDetailsPage.ts file

--- pages/houseDetailsPage.ts
+++ pages/houseDetailsPage.ts
@@ -1,4 +1,4 @@
-import { Page, Locator, expect } from "@playwright/test";
+import { Page, Locator } from "@playwright/test";
 import { LocalizationHelper } from "../utilities/localizationHelper";
 
 export class HouseDetailsPage {
@@ -16,19 +16,45 @@ export class HouseDetailsPage {
     //House Agency Locators
     houseAgencyName: (agencyName: string) =>
       this.page.locator("h3").getByTitle(agencyName),
-    contactAgencyButton: () =>
-      this.page
+    contactAgencyButton: () => {
+      const showTelephone = LocalizationHelper.getLocalizedText(
+        "buttons",
+        "showTelephone"
+      );
+      return this.page
         .getByRole("button")
-        .filter({ hasText: "Toon telefoonnummer" })
-        .first(),
+        .filter({ hasText: showTelephone })
+        .first();
+    },
     houseAgencyTelephoneNumber: (agencyTelephone: string) =>
       this.page.getByRole("button", { name: agencyTelephone }),
+    agencyContactUsButton: () => {
+      const contact = LocalizationHelper.getLocalizedText("buttons", "contact");
+      return this.page.getByRole("link", { name: contact }).first();
+    },
+    agencyRequestViewingButton: () => {
+      const requestViewing = LocalizationHelper.getLocalizedText(
+        "buttons",
+        "requestViewing"
+      );
+      return this.page.getByRole("link", { name: requestViewing }).first();
+    },
 
     // Saving & unsaving to Favorites Locators
-    saveToFavoritesButton: () =>
-      this.page.getByRole("button", { name: "Bewaren" }).first(),
-    alreadySavedToFavoritesButton: () =>
-      this.page.locator('button[title="Huis verwijderen"]').first(),
+    saveToFavoritesButton: () => {
+      const saveToFavorite = LocalizationHelper.getLocalizedText(
+        "buttons",
+        "saveToFavorite"
+      );
+      return this.page.getByRole("button", { name: saveToFavorite }).first();
+    },
+    alreadySavedToFavoritesButton: () => {
+      const removeFromFavorite = LocalizationHelper.getLocalizedText(
+        "buttons",
+        "removeFromFavorite"
+      );
+      return this.page.getByTitle(removeFromFavorite).first();
+    },
     favoriteSuccessMessage: () => this.page.locator("h4.font-semibold"),
     favoritesLink: () => this.page.locator('a[href*="favorieten/"]').first(),
   };
@@ -36,12 +62,18 @@ export class HouseDetailsPage {
   constructor(page: Page) {
     this.page = page;
   }
-
   async navigateToHouseDetails(houseUrl: string): Promise<void> {
     await this.page.goto(houseUrl);
     console.log("House details page has been opened.");
   }
-
+  async openAgencyContactUsPage(): Promise<void> {
+    await this.locators.agencyContactUsButton().click();
+    console.log("Agency Contact page has been opened.");
+  }
+  async openRequestVieweingPage(): Promise<void> {
+    await this.locators.agencyRequestViewingButton().click();
+    console.log("Request a viewing page has been opened.");
+  }
   async saveToFavorites(): Promise<void> {
     if (await this.locators.alreadySavedToFavoritesButton().isVisible()) {
       await this.locators.alreadySavedToFavoritesButton().click();
@@ -53,47 +85,33 @@ export class HouseDetailsPage {
     await this.locators.saveToFavoritesButton().click();
     console.log("House has been saved to favorites.");
   }
-
   async removeromFavorites(): Promise<void> {
     await this.locators.alreadySavedToFavoritesButton().click();
     console.log("House has been removed from favorites.");
   }
-
-  async verifyHouseDetailsInfo(
-    address: string,
-    postalCode: string,
-    askingPrice: string
-  ): Promise<void> {
-    await expect(this.locators.houseAddress(address)).toBeVisible();
-    console.log("House Address is verified");
-    await expect(this.locators.housePostalCode(postalCode)).toBeVisible();
-    console.log("House Postal Code is verified");
-    await expect(this.locators.houseAskingPrice(askingPrice)).toBeVisible();
-    console.log("House Asking price is verified");
-  }
-
-  async verifyAgencyDetails(
-    agencyName: string,
-    agencyTelephone: string
-  ): Promise<void> {
-    await expect(this.locators.houseAgencyName(agencyName)).toBeVisible();
-    console.log("House Agency Name is verified");
+  async clickContactAgencyButton(): Promise<void> {
     await this.locators.contactAgencyButton().click();
-    await expect(
-      this.locators.houseAgencyTelephoneNumber(agencyTelephone)
-    ).toBeVisible();
-    console.log("House Agency Telephonenumber is verified");
+    console.log("Contact Button is clicked and telephone number is shown.");
   }
-
-  async verifyFavoriteSuccessMessage(language: string): Promise<void> {
-    const expectedMessage = LocalizationHelper.getLocalizedText(
-      language,
-      "favoriteSuccessMessage"
-    );
-    const link = this.locators.favoritesLink();
-    await expect(this.locators.favoriteSuccessMessage()).toHaveText(
-      expectedMessage
-    );
-    await expect(link).toHaveText("Favorieten");
+  getHouseAddress(address: string) {
+    return this.locators.houseAddress(address);
+  }
+  getPostalCode(postalCode: string) {
+    return this.locators.housePostalCode(postalCode);
+  }
+  getAskingPrice(askingPrice: string) {
+    return this.locators.houseAskingPrice(askingPrice);
+  }
+  getAgencyName(agencyName: string) {
+    return this.locators.houseAgencyName(agencyName);
+  }
+  getAgencyTelephone(agencyTelephone: string) {
+    return this.locators.houseAgencyTelephoneNumber(agencyTelephone);
+  }
+  get favoriteSuccessMessage(): Locator {
+    return this.locators.favoriteSuccessMessage();
+  }
+  get favoritesLink(): Locator {
+    return this.locators.favoritesLink();
   }
 }
