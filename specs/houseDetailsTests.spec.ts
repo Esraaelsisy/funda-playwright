86d0489623ea6dc6a7e10f2649873b37a978603e
Esraaelsisy <esraa.elsisy@gmail.com>
2025-01-19 23:19:11 +0100

    Move the asserations to the tests in houseDetailsTests.spec.ts file

--- specs/houseDetailsTests.spec.ts
+++ specs/houseDetailsTests.spec.ts
@@ -2,64 +2,94 @@ import { test, expect } from "../fixtures/baseTest";
 import { authTest, authExpect } from "../fixtures/authTest";
 import { PageManager } from "../pages/pageManager";
 import testData from "../data/testData.json";
+import { LocalizationHelper } from "../utilities/localizationHelper";
 
 test.describe("House Details Page Tests", () => {
-  test("Verify house details page loads successfully", async ({ page }) => {
+  test("House details page loads successfully", async ({ page }) => {
     const pm = new PageManager(page);
 
+    //Action: Navigate to the house details page
     await test.step("Navigate to the house details page", async () => {
       await pm.houseDetailsPage.navigateToHouseDetails(
         testData.house.detailsUrl
       );
     });
-
-    await test.step("Verify house details are displayed correctly", async () => {
-      await pm.houseDetailsPage.verifyHouseDetailsInfo(
-        testData.house.address,
-        testData.house.postalCode,
-        testData.house.askingPrice
-      );
+    //Assertions
+    await test.step("Verify house address", async () => {
+      await expect(
+        pm.houseDetailsPage.getHouseAddress(testData.house.address)
+      ).toBeVisible();
+      console.log("House Address is verified");
     });
 
-    await test.step("Verify agency details are displayed correctly", async () => {
-      await pm.houseDetailsPage.verifyAgencyDetails(
-        testData.house.agencyName,
-        testData.house.agencyContact
-      );
-    });
-  });
-
-  authTest("Verify saving a house to favorites", async ({ page }) => {
-    const pm = new PageManager(page);
-
-    await authTest.step("Navigate to the house details page", async () => {
-      await pm.houseDetailsPage.navigateToHouseDetails(
-        testData.house.detailsUrl
-      );
+    await test.step("Verify house postal code", async () => {
+      await expect(
+        pm.houseDetailsPage.getPostalCode(testData.house.postalCode)
+      ).toBeVisible();
+      console.log("House Postal Code is verified");
     });
 
-    await authTest.step("Save the house to favorites", async () => {
-      await pm.houseDetailsPage.saveToFavorites();
+    await test.step("Verify house asking price", async () => {
+      await expect(
+        pm.houseDetailsPage.getAskingPrice(testData.house.askingPrice)
+      ).toBeVisible();
+      console.log("House Asking Price is verified");
     });
-    await authTest.step(
-      "Verify favorite success message and Favorites Link",
-      async () => {
-        await pm.houseDetailsPage.verifyFavoriteSuccessMessage("NL");
-      }
-    );
 
-    await authTest.step("Remove this house from favorites ", async () => {
-      await pm.houseDetailsPage.removeromFavorites();
+    await test.step("Verify agency name is visible", async () => {
+      await expect(
+        pm.houseDetailsPage.getAgencyName(testData.house.agencyName)
+      ).toBeVisible();
+      console.log("House Agency Name is verified");
     });
-  });
 
-  test("Verify real estate agency details", async ({ page }) => {
-    const pm = new PageManager(page);
-
-    await test.step("Navigate to the house details page", async () => {
-      await pm.houseDetailsPage.navigateToHouseDetails(
-        testData.house.detailsUrl
-      );
+    await test.step("Verify agency telephone number is visible", async () => {
+      await pm.houseDetailsPage.clickContactAgencyButton();
+      await expect(
+        pm.houseDetailsPage.getAgencyTelephone(
+          testData.house.agencyContact
+        )
+      ).toBeVisible();
+      console.log("House Agency Telephone number is verified");
     });
   });
+
+  authTest(
+    "Saving a house to favorites with logged in user",
+    async ({ page }) => {
+      const pm = new PageManager(page);
+      //Action: Navigate to the house details page
+      await authTest.step("Navigate to the house details page", async () => {
+        await pm.houseDetailsPage.navigateToHouseDetails(
+          testData.house.detailsUrl
+        );
+      });
+      //Action: Save the house to Favorites
+      await authTest.step("Save the house to favorites", async () => {
+        await pm.houseDetailsPage.saveToFavorites();
+      });
+      //Assertions
+      await authTest.step(
+        "Verify favorite success message and Favorites Link",
+        async () => {
+          const expectedMessage = LocalizationHelper.getLocalizedText(
+            "messages",
+            "favoriteSuccessMessage"
+          );
+          await expect(pm.houseDetailsPage.favoriteSuccessMessage).toHaveText(
+            expectedMessage
+          );
+          console.log("Favorite Success Message is shown.");
+          await expect(pm.houseDetailsPage.favoritesLink).toHaveText(
+            "Favorieten"
+          );
+          console.log("Favorite Success Message has Favorites Link.");
+        }
+      );
+      //Action : Remove the house from favorites
+      await authTest.step("Remove this house from favorites ", async () => {
+        await pm.houseDetailsPage.removeromFavorites();
+      });
+    }
+  );
 });
