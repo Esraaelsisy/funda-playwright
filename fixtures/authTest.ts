ab17df21d2bc608c9cf78adaa42c0c4b932bbe94
Esraaelsisy <esraa.elsisy@gmail.com>
2025-01-19 02:19:02 +0100

    Reading the username and password from .env file for security

--- fixtures/authTest.ts
+++ fixtures/authTest.ts
@@ -1,15 +1,21 @@
 import { test as base, expect as baseExpect } from "./baseTest";
 import { PageManager } from "../pages/pageManager";
-import testData from "../data/testData.json";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const authTest = base.extend<{}>({
   page: async ({ page }, use) => {
     const pm = new PageManager(page);
-    const { username, password } = testData.credentials.valid;
+    const { USERNAME: username, PASSWORD: password } = process.env; 
+    if (!username || !password) {
+      throw new Error('Environment variables USERNAME and PASSWORD must be set in .env');
+    }
     await pm.homePage.navigateToLoginPage();
     await pm.loginPage.login(username, password);
     await pm.homePage.isAccountMenuVisible();
     await use(page);
   },
 });
+
 export { baseExpect as authExpect };
